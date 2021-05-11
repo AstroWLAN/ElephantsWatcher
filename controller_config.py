68ee7af0bf783ab9ced60c416469581ecfbffb6e
AstroWLAN <dario2.crippa@mail.polimi.it>
2021-05-11 16:00:51 +0200

    Added support for the user interaction at the controller startup

--- controller_config.py
+++ controller_config.py
@@ -4,6 +4,27 @@ from ryu.controller import ofp_event
 from ryu.controller.handler import set_ev_cls, CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.ofproto import ofproto_v1_3
 
+# GLOBAL VARIABLES
+# Escape sequences
+ansiWhite = u'\u001b[38;5;231m'
+ansiRed = u'\u001b[38;5;197m'
+ansiRST = u'\u001b[0m'
+ansiBlue = u'\u001b[38;5;39m'
+# Handles user preferences
+userChoice = input(ansiWhite + '❗️ Do you want to enable the COUNT-MIN SKETCHES algorythm? [y/n]\n>> ').lower()
+# Wrong input
+while userChoice != 'y' and userChoice != 'n':
+    userChoice = input(ansiRed + 'Bad input...retry!\n' + ansiWhite + '>> ').lower()
+print(ansiRST)
+# FLOWMANAGER url
+print(ansiRed + 'FLOWMANAGER' + ansiWhite + ' : ' + ansiBlue + 'http://localhost:8080/home/index.html\n' + ansiRST)
+
+# CMS VARIABLES
+# CMS Threshold | Each iperf TCP packet is about 8KB so we set the threshold at 512MB
+thresholdCMS = 65536
+arrayCMS = None
+arrayCMSLength = 10
+
 
 # CONTROLLER | Elephants Watcher
 class ElephantWatcher(app_manager.RyuApp):
