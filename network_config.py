a78352b73750825e7f11189a088ec3ddae248c0b
AstroWLAN <dario2.crippa@mail.polimi.it>
2021-05-16 00:51:30 +0200

    Updated network_config.py : Added support for the switch rules visualization at the end of the simulation

--- network_config.py
+++ network_config.py
@@ -1,6 +1,5 @@
 # Python modules
 from mininet.cli import CLI
-from mininet.link import TCLink
 from mininet.log import setLogLevel
 from mininet.net import Mininet
 from mininet.node import RemoteController
@@ -20,7 +19,8 @@ portMaxValue = 65536
 ansiWhite = u'\u001b[38;5;231m'
 ansiRed = u'\u001b[38;5;197m'
 ansiRST = u'\u001b[0m'
-ansiBlue = u'\u001b[38;5;39'
+ansiBlue = u'\u001b[38;5;39m'
+ansiDarkGray = u'\u001b[38;5;243m'
 
 
 # TOPOLOGY
@@ -67,10 +67,26 @@ class IntrastellarTopology(Topo):
         self.addLink(switchD, switchA)
 
 
+# SUPPORT FUNCTIONS
+# Visualizes the CLI
+def displayCLI(network):
+    userChoice = input(ansiWhite + '\n*** ❗️ Do you want to use the CLI? [y/n]\n' + ansiRST + '> ').lower()
+    while userChoice != 'y' and userChoice != 'n':
+        userChoice = input(ansiRed + 'Bad input...retry!\n' + ansiWhite + '>> ').lower()
+    print(ansiRST)
+    if userChoice == 'y':
+        # Launches the CLI for further commands
+        print(ansiRed + '⚙️   COMMAND LINE' + ansiWhite + ' : Enables the manual usage for further commands\n\n' +
+              ansiDarkGray + '                  - Visualizes the switch rules :' + ansiWhite + ' dpctl dump-flows\n' +
+              ansiDarkGray + '                  - Ends the simulation :' + ansiWhite + ' exit\n' +
+              ansiRST)
+        CLI(network)
+
+
 # TESTS
 # Performs a simple ping test to verify the reachability of all the nodes in the network
 def reachabilityTest(network):
-    print(u"\u001b[38;5;197m\n⚙️   TEST" + u"\u001b[38;5;231m : Hosts reachability" + u"\u001b[0m\n")
+    print(u"\u001b[38;5;197m\n🔦️  REACHABILITY" + u"\u001b[38;5;231m : Hosts reachability" + u"\u001b[0m\n")
     # Performs at most five pingAll tests
     attemptsCounter = 0
     dropRate = 100.0
@@ -91,10 +107,10 @@ def reachabilityTest(network):
 
 # Generates traffic through the network
 def trafficTest(network):
-    print(u"\u001b[38;5;197m\n🚐  TRAFFIC" +
-          u"\u001b[38;5;231m : Generates traffic through the network\n    Dashboard : " +
+    print(u"\u001b[38;5;197m🚐  TRAFFIC" +
+          u"\u001b[38;5;231m : Sends packets through the network\n    Visualize the traffic at : " +
           u"\u001b[38;5;39mhttps://tinyurl.com/3zwb9c2n\n" +
-          u"\u001b[0m\n")
+          u"\u001b[0m")
     # TRAFFIC
     portList = list(range(portMinValue, portMaxValue))
     # Gets the nodes
@@ -109,16 +125,18 @@ def trafficTest(network):
     portList.remove(mousePort)
     # Configures the nodes
     mouseServerCommand = 'iperf -s' + ' -p ' + str(mousePort) + ' & '
-    # Transmits 1 MB | It's a mouse... very few packets
-    mouseClientCommand = 'iperf -c ' + serverMouse.IP() + ' -p ' + str(mousePort) + ' -n ' + '100M'
-    print(u"\u001b[38;5;231m\n*** The mouse is on his way... | 100 MByte" + u"\u001b[0m\n")
+    # Transmits 100 MB | It's a mouse... very few packets
+    mouseClientCommand = 'iperf -c ' + serverMouse.IP() + ' -p ' + str(mousePort) + ' -n ' + '1M'
+    mouseClientCommand += ' -w ' + '10M'
+    print(u"\u001b[38;5;231m*** ❗ The mouse is on his way... | 1 MB" + u"\u001b[0m")
     serverMouse.cmdPrint(mouseServerCommand)
     clientMouse.cmdPrint(mouseClientCommand)
 
     elephantServerCommand = 'iperf -s' + ' -p ' + str(elephantPort) + ' & '
-    # Transmits 10 GB | It's an elephant...lots of packets!
-    elephantClientCommand = 'iperf -c ' + serverElephant.IP() + ' -p ' + str(elephantPort) + ' -n ' + '1G'
-    print(u"\u001b[38;5;231m*** The elephant is on his way... | 1 GByte" + u"\u001b[0m")
+    # Transmits 1 GB | It's an elephant...lots of packets!
+    elephantClientCommand = 'iperf -c ' + serverElephant.IP() + ' -p ' + str(elephantPort) + ' -n ' + '100M'
+    elephantClientCommand += ' -w ' + '10M'
+    print(u"\u001b[38;5;231m\n*** ❗ The elephant is on his way... | 100 MB" + u"\u001b[0m")
     serverElephant.cmdPrint(elephantServerCommand)
     clientElephant.cmdPrint(elephantClientCommand)
 
@@ -138,7 +156,7 @@ if __name__ == '__main__':
     topology = IntrastellarTopology()
     # Creates a Mininet network based on the defined topology
     # autoSetMacs | --mac & autoStaticArp | --arp
-    net = Mininet(topology, controller=None, autoSetMacs=True, autoStaticArp=False, link=TCLink)
+    net = Mininet(topology, controller=None, autoSetMacs=True, autoStaticArp=False)
     # Adds a custom remote controller | IP : 127.0.0.1 is the LocalHost while Port : 6633 is the default Ryu port
     net.addController('elephantsWatcher', controller=RemoteController, ip='127.0.0.1', port=6633)
     # Starts the network
@@ -146,14 +164,6 @@ if __name__ == '__main__':
     dumpNodeConnections(net.hosts)
     reachabilityTest(net)
     trafficTest(net)
-
-    userChoice = input(ansiWhite + '❗️ Do you wanna use the CLI? [y/n]\n>> ').lower()
-    while userChoice != 'y' and userChoice != 'n':
-        userChoice = input(ansiRed + 'Bad input...retry!\n' + ansiWhite + '>> ').lower()
-    print(ansiRST)
-    if userChoice == 'y':
-        # Launches the CLI for further commands
-        CLI(net)
-    else:
-        # Stops the network
-        net.stop()
+    displayCLI(net)
+    # Stops the network
+    net.stop()
